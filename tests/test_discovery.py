d9d6d4b52def672057182ebe585eb216eb49b5fd
alexshe <alexshe@wix.com>
2018-08-20 16:49:36 +0300

    conf fixes

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -6,6 +6,6 @@ from chaosk8s_wix  import __version__, discover
 
 def test_discover_extension_capabilities():
     discovery = discover(discover_system=False)
-    assert discovery["extension"]["name"] == "chaostoolkit-kubernetes"
+    assert discovery["extension"]["name"] == "chaostoolkit-k8s-wix"
     assert discovery["extension"]["version"] == __version__
     assert len(discovery["activities"]) > 0
